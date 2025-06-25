3109a6b846741bd73975bd76c5a29f1fc1e8ec07
nelitonps <neliton.net@hotmail.com>
2025-06-25 15:01:43 -0300

    Initial commit

---
+++ src/main/java/com/nelitonps/chamadosAT/ChamadosAtApplication.java
@@ -0,0 +1,13 @@
+package com.nelitonps.chamadosAT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChamadosAtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChamadosAtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nelitonps/chamadosAT/ChamadosAtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nelitonps.chamadosAT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChamadosAtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
