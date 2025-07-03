53f9ea4697c3fe74a55c2238785818fcfce86350
nelitonps <neliton.net@hotmail.com>
2025-07-03 16:52:28 -0300

    classes de excecao criadas

---
+++ src/main/java/com/nelitonps/chamadosAT/services/exception/DataIntegrityViolationException.java
@@ -0,0 +1,13 @@
+package com.nelitonps.chamadosAT.services.exception;
+
+public class DataIntegrityViolationException extends RuntimeException{
+    private static final long serialVersionUID = 1L;
+
+    public DataIntegrityViolationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public DataIntegrityViolationException(String message) {
+        super(message);
+    }
+}
